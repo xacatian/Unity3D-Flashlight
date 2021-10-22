9b983e585148023b63306b8bc03693539aea5617
Olgun Alper Keskin <92810200+xacatian@users.noreply.github.com>
2021-10-22 23:42:14 +0300

    Add files via upload

---
+++ flashlight.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class flashlight : MonoBehaviour
+{
+    private bool aciksa;
+    public GameObject flashlight1;
+
+    private void Start()
+    {
+        aciksa = true;
+    }
+
+    private void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            if (aciksa)
+            {
+                flashlight1.GetComponent<Light>().enabled = false;
+                aciksa = false;
+            }
+            else
+            {
+                flashlight1.GetComponent<Light>().enabled = true;
+                aciksa = true;
+            }
+        }
+    }
+}
